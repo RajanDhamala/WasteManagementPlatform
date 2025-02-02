16d3984e9c3f63b48054c6474092fd3fbf969e75
rajan <rajandhamala0123@gmail.com>
2025-02-02 08:56:14 +0545

    change in routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,27 +1,16 @@
 import app from './app.js';
 import dotenv from 'dotenv';
 import ConnectDb from './src/Database/ConnectDb.js';
-import { createServer } from "http";
 
 dotenv.config();
 
-let isConnected = false;
+ConnectDb().then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.log('Error in connecting to the database',err);
+})
 
-async function ensureDbConnection() {
-    if (!isConnected) {
-        try {
-            await ConnectDb();
-            isConnected = true;
-            console.log("✅ Connected to the database!");
-        } catch (err) {
-            console.error("❌ Database connection error:", err);
-        }
-    }
-}
 
-const server = createServer(app);
 
-export default async function handler(req, res) {
-    await ensureDbConnection(); // Ensure DB is connected
-    return server.emit("request", req, res);
-}
